84cdfd54d93e7a98b92b6aba5f42eb90213d04c8
moriagr <moriagro&gmail.com>
2023-03-13 18:50:31 +0200

    initial commit

---
+++ AdptArray.c
@@ -0,0 +1,102 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "AdptArray.h"
+
+typedef struct AdptArray_
+{
+	int ArrSize;
+	PElement *pElemArr;
+	DEL_FUNC delFunc;
+	COPY_FUNC copyFunc;
+	PRINT_FUNC printFunc;
+} AdptArray;
+
+PAdptArray CreateAdptArray(COPY_FUNC _copyFunc, DEL_FUNC _delFunc, PRINT_FUNC _printFunc)
+{
+	PAdptArray pArr = (PAdptArray)malloc(sizeof(AdptArray));
+	if (pArr == NULL)
+	{
+		return NULL;
+	}
+	pArr->ArrSize = 0;
+	pArr->pElemArr = NULL;
+	pArr->copyFunc = _copyFunc;
+	pArr->delFunc = _delFunc;
+	pArr->printFunc = _printFunc;
+	return pArr;
+}
+void DeleteAdptArray(PAdptArray pArr)
+{
+	int i;
+	if (pArr == NULL)
+		return;
+	for (i = 0; i < pArr->ArrSize; ++i)
+	{
+		pArr->delFunc((pArr->pElemArr)[i]);
+	}
+	free(pArr->pElemArr);
+	free(pArr);
+}
+
+Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem)
+{
+	PElement *newpElemArr;
+	if (pArr == NULL)
+		return FAIL;
+
+	if (idx >= pArr->ArrSize)
+	{
+
+		// Extend Array
+		if ((newpElemArr = (PElement *)calloc((idx + 1), sizeof(PElement))) == NULL)
+			return FAIL;
+		memcpy(newpElemArr, pArr->pElemArr, (pArr->ArrSize) * sizeof(PElement));
+		free(pArr->pElemArr);
+		pArr->pElemArr = newpElemArr;
+	}
+
+	// Delete Previous Elem
+
+	PElement *temp = (pArr->pElemArr)[idx];
+	free((pArr->pElemArr)[idx]);
+	pArr->delFunc(temp);
+
+	(pArr->pElemArr)[idx] = pArr->copyFunc(pNewElem);
+
+	// Update Array Size
+	pArr->ArrSize = (idx >= pArr->ArrSize) ? (idx + 1) : pArr->ArrSize;
+	return SUCCESS;
+}
+
+PElement GetAdptArrayAt(PAdptArray pArr, int index)
+{
+	PAdptArray pCurrArr = pArr;
+
+	PElement newpElemArr;
+	if (pArr == NULL || pArr->ArrSize <= index)
+		return FAIL;
+
+	if ((newpElemArr = (PElement *)malloc(sizeof(PElement))) == NULL)
+		return FAIL;
+	int count = 0;
+	while (count < index)
+	{
+		count++;
+		// pCurrArr = pCurrArr->pElemArr;
+	}
+	newpElemArr = pCurrArr->copyFunc((pCurrArr->pElemArr)[count]);
+	return newpElemArr;
+}
+
+int GetAdptArraySize(PAdptArray pArr)
+{
+	if (pArr->ArrSize < 0 || pArr->copyFunc == NULL || pArr->delFunc == NULL || pArr->printFunc == NULL)
+		return -1;
+	return pArr->ArrSize;
+}
+
+void PrintDB(PAdptArray pArr)
+{
+}
